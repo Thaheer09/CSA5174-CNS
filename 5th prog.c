822cd87059a4148428f683f3517eedca2118ccb5
Taheer <125257563+Thaheer09@users.noreply.github.com>
2024-11-07 10:08:24 +0530

    Add files via upload

---
+++ 5th prog.c
@@ -0,0 +1,48 @@
+{
+  "cells" : [
+    {
+      "content" : "## New Notebook\n---\nClick to edit this.",
+      "creationTime" : 1730953032,
+      "identifier" : "81CE6051-F933-47F1-A3F9-B9B1D40CE9EC",
+      "isCollapsed" : false,
+      "mutateTime" : 1730953032,
+      "type" : "markdown"
+    },
+    {
+      "content" : "\/\/ all\n#include <stdio.h>\n#include <stdlib.h>\n#include <string.h>\n#include <math.h>\n#include <time.h>\n#include <unistd.h>\n",
+      "creationTime" : 1730953032,
+      "identifier" : "4A358EA1-C8FC-4F4B-8BD0-6D04721E191E",
+      "isCollapsed" : false,
+      "mutateTime" : 1730953032,
+      "type" : "code"
+    },
+    {
+      "content" : "The first line of the code cell **define the code's name** (if first line is comment), that can be include by others cell. Click ▶️ to run the code cell.",
+      "creationTime" : 1730953032,
+      "identifier" : "671B2346-7767-4A4F-8E13-9D80B40D0ED2",
+      "isCollapsed" : false,
+      "mutateTime" : 1730953032,
+      "type" : "markdown"
+    },
+    {
+      "content" : "#include <all>\nfor (int i = 0; i < 3; i++) {\n    printf(\"Hello world %d\\n\", i);\n    sleep(1);\n}\n",
+      "creationTime" : 1730953032,
+      "identifier" : "0C9E4615-5910-418D-9959-E6614E096529",
+      "isCollapsed" : false,
+      "mutateTime" : 1730953032,
+      "type" : "code"
+    }
+  ],
+  "codeResources" : "2a0219fac41b28cafd4f45e67dfa3d9ca24d0fd04e053b802680f4af773b9129",
+  "creationTime" : 1730953032,
+  "executeEngine" : "ccdinterp",
+  "identifier" : "3F216539-B6BB-45EB-BCD7-C93DB7629CDA",
+  "latestOpenedTools" : "notebook",
+  "latestOpenedToolsBuildId" : 551,
+  "latestOpenedToolsVersion" : "3.7.0",
+  "mutateTime" : 1730953032,
+  "tools" : "notebook",
+  "toolsBuildId" : 551,
+  "toolsVersion" : "3.7.0",
+  "version" : 1
+}
